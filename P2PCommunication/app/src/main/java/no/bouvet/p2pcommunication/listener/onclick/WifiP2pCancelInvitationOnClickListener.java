c6cd3ab3b79ae3de261e9fad1702ddf7140945e0
Taardal <Torbjorn.aardal@gmail.com>
2015-03-05 14:31:13 +0100

    User can now cancel connection invitation

---
+++ P2PCommunication/app/src/main/java/no/bouvet/p2pcommunication/listener/onclick/WifiP2pCancelInvitationOnClickListener.java
@@ -0,0 +1,19 @@
+package no.bouvet.p2pcommunication.listener.onclick;
+
+import android.view.View;
+
+import no.bouvet.p2pcommunication.listener.WifiP2pListener;
+
+public class WifiP2pCancelInvitationOnClickListener implements View.OnClickListener {
+
+    private WifiP2pListener wifiP2pListener;
+
+    public WifiP2pCancelInvitationOnClickListener(WifiP2pListener wifiP2pListener) {
+        this.wifiP2pListener = wifiP2pListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        wifiP2pListener.onCancelConnect();
+    }
+}
--- P2PCommunication/app/src/main/java/no/bouvet/p2pcommunication/listener/wifip2paction/WifiP2pConnectActionListener.java
+++ P2PCommunication/app/src/main/java/no/bouvet/p2pcommunication/listener/wifip2paction/WifiP2pConnectActionListener.java
@@ -6,19 +6,23 @@ import android.util.Log;
 import android.widget.Toast;
 
 import no.bouvet.p2pcommunication.R;
+import no.bouvet.p2pcommunication.listener.state.ConnectionStateListener;
 import no.bouvet.p2pcommunication.wifip2p.P2pCommunicationWifiP2pManager;
 
 public class WifiP2pConnectActionListener implements WifiP2pManager.ActionListener {
 
     private static final String TAG = WifiP2pConnectActionListener.class.getSimpleName();
-    private final Context context;
+    private Context context;
+    private ConnectionStateListener connectionStateListener;
 
-    public WifiP2pConnectActionListener(Context context) {
+    public WifiP2pConnectActionListener(Context context, ConnectionStateListener connectionStateListener) {
         this.context = context;
+        this.connectionStateListener = connectionStateListener;
     }
 
     @Override
     public void onSuccess() {
+        connectionStateListener.onInvitationToConnectSent();
         Log.i(TAG, context.getString(R.string.successfully_sent_invitation_to_connect));
     }
 
